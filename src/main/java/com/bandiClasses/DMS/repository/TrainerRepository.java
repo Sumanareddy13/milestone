c58d1c3231ff39fc11a518778d67b43f8e2d59b0
Sumanasri Reddy <pailasumanasri@gmail.com>
2023-11-25 22:03:01 -0600

    intial commit

---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,12 @@
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+/**
+* Sumanasri Paila
+*/
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
